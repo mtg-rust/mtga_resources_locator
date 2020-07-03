556d8f1f77aa144dca37a88c1e0269cce553368c
Michele Gargiulo <michele@grg.io>
2020-07-03 14:57:17 +0200

    Missing error check

--- src/path_locator/windows.rs
+++ src/path_locator/windows.rs
@@ -8,7 +8,7 @@ use winreg::RegKey;
 
 pub fn assets_data_dir() -> io::Result<PathBuf> {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let mtga_reg_subkey = hklm.open_subkey(r"SOFTWARE\WOW6432Node\Wizards of the Coast\MTGArena");
+    let mtga_reg_subkey = hklm.open_subkey(r"SOFTWARE\WOW6432Node\Wizards of the Coast\MTGArena")?;
     let path_reg_key: String = mtga_reg_subkey.get_value("Path")?;
 
     Ok(PathBuf::from(path_reg_key))
